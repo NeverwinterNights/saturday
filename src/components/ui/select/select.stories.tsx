5dae0bcb781292c8b9a1a5ec3d0326d3029338f9
Igor Shargin <igor20513@gmail.com>
2023-07-03 12:57:31 +0500

    feat(Select): add stories

---
+++ src/components/ui/select/select.stories.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+
+import type { Meta } from '@storybook/react'
+
+import { Select, SelectProps } from './'
+
+export default {
+  title: 'Components/Select',
+  component: Select,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Select>
+
+const optionsPrimary = [
+  {
+    value: 'apple',
+    label: 'Apple',
+  },
+  {
+    value: 'banana',
+    label: 'Banana',
+  },
+  {
+    value: 'blueberry',
+    label: 'Blueberry',
+  },
+  {
+    value: 'grapes',
+    label: 'Grapes',
+  },
+]
+const optionsPagination = [
+  {
+    value: '1',
+    label: '1',
+  },
+  {
+    value: '2',
+    label: '2',
+  },
+  {
+    value: '3',
+    label: '3',
+  },
+  {
+    value: '4',
+    label: '4',
+  },
+]
+
+export const Simple = {
+  render: (args: SelectProps) => {
+    const [value, setValue] = useState('')
+
+    return <Select {...args} value={value} onChange={setValue} />
+  },
+
+  args: {
+    options: optionsPrimary,
+  },
+}
+
+export const SimpleWithLabel = {
+  render: (args: SelectProps) => {
+    const [value, setValue] = useState('')
+
+    return <Select {...args} value={value} onChange={setValue} />
+  },
+
+  args: {
+    placeholder: 'select...',
+    options: optionsPrimary,
+    label: 'Select',
+  },
+}
+
+export const Error = {
+  render: (args: SelectProps) => {
+    const [value, setValue] = useState('')
+
+    return <Select {...args} value={value} onChange={setValue} />
+  },
+
+  args: {
+    placeholder: 'select...',
+    options: optionsPrimary,
+    label: 'Select',
+    errorMessage: 'error',
+  },
+}
+export const Pagination = {
+  render: (args: SelectProps) => {
+    const [value, setValue] = useState('')
+
+    return <Select {...args} value={value} onChange={setValue} />
+  },
+
+  args: {
+    options: optionsPagination,
+    label: 'pagination',
+    variant: 'pagination',
+    placeholder: '1',
+  },
+}
