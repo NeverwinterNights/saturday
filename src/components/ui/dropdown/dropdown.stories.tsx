ea05bdae6740414288074ba3fe2438ecfc1f49a2
Yelkira <penda.fu@gmail.com>
2023-07-03 22:04:19 +0300

    css and stories for dropdown done

---
+++ src/components/ui/dropdown/dropdown.stories.tsx
@@ -0,0 +1,96 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Dropdown, DropdownItemWithIcon } from '.'
+
+import { Edit } from '../../../assets/icons/Edit.tsx'
+import { Play } from '../../../assets/icons/Play.tsx'
+import { Trash } from '../../../assets/icons/Trash.tsx'
+import { Button } from '../button'
+
+const meta = {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+} satisfies Meta<typeof Dropdown>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: args => {
+    return (
+      <div>
+        <Dropdown {...args}>
+          <>
+            <DropdownItemWithIcon icon={<Play />} text="Learn" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<Edit />} text="Edit" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<Trash />} text="Delete" onSelect={() => {}} />
+          </>
+        </Dropdown>
+      </div>
+    )
+  },
+}
+export const WithAlign: Story = {
+  render: args => {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Dropdown {...args}>
+          <>
+            <DropdownItemWithIcon icon={<Play />} text="Learn" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<Edit />} text="Edit" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<Trash />} text="Delete" onSelect={() => {}} />
+          </>
+        </Dropdown>
+      </div>
+    )
+  },
+  args: {
+    children: <></>,
+    align: 'center',
+  },
+}
+export const AllDisabled: Story = {
+  render: args => {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Dropdown {...args}>
+          <>
+            <DropdownItemWithIcon
+              disabled={true}
+              icon={<Play />}
+              text="Learn"
+              onSelect={() => {}}
+            />
+            <DropdownItemWithIcon disabled={true} icon={<Edit />} text="Edit" onSelect={() => {}} />
+            <DropdownItemWithIcon
+              disabled={true}
+              icon={<Trash />}
+              text="Delete"
+              onSelect={() => {}}
+            />
+          </>
+        </Dropdown>
+      </div>
+    )
+  },
+  args: {
+    children: <></>,
+    align: 'center',
+  },
+}
+
+export const WithTriggerButton: Story = {
+  render: args => {
+    return (
+      <div>
+        <Dropdown trigger={<Button>Trigger?</Button>} {...args}>
+          <>
+            <DropdownItemWithIcon icon={<Play />} text="Learn" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<Edit />} text="Edit" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<Trash />} text="Delete" onSelect={() => {}} />
+          </>
+        </Dropdown>
+      </div>
+    )
+  },
+}
