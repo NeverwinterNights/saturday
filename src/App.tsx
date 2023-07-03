01412955c389a1edcfb8128306b5d9d0138980ab
Igor Shargin <igor20513@gmail.com>
2023-07-03 18:38:14 +0500

    feat(RadioGroup): add radio-group component

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,7 @@ export function App() {
           options={optionsPagination}
           value={valuePagination}
           onChange={e => setValuePagination(e)}
+          width={'100%'}
         />
       </div>
       <Typography variant="large" as="h1">
