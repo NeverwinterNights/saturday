3af400b82238e1a5746dfea59b25973bd1bbf259
pavelcardash <cardash@mail.ru>
2023-07-05 20:58:57 +0300

    done check mail

---
+++ src/components/auth/checkMail/check-mail.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { CheckMail } from './index.ts'
+
+const meta = {
+  title: 'Components/CheckMail',
+  component: CheckMail,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof CheckMail>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CheckMailMain: Story = {
+  args: { email: 'sobaka@sobaka.by' },
+}
