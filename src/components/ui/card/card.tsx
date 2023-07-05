aa606672532b6970c45cc6fc9802ef112115b863
Yelkira <penda.fu@gmail.com>
2023-07-05 23:18:01 +0300

    fix className in card component

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,9 +1,15 @@
 import { ComponentPropsWithoutRef, forwardRef } from 'react'
 
-import styles from './card.module.scss'
+import { clsx } from 'clsx'
+
+import s from './card.module.scss'
 
 type CardProp = {} & ComponentPropsWithoutRef<'div'>
 
 export const Card = forwardRef<HTMLDivElement, CardProp>(({ className, ...restProps }, ref) => {
-  return <div ref={ref} className={styles.container} {...restProps}></div>
+  const classNames = {
+    root: clsx(s.root, className),
+  }
+
+  return <div ref={ref} className={classNames.root} {...restProps}></div>
 })
