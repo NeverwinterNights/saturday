96ee50e5e4a872ee12a421fddc979bcdda08cd27
Yelkira <penda.fu@gmail.com>
2023-07-10 20:40:13 +0300

    add star icons

---
+++ src/assets/icons/StarEmpty.tsx
@@ -0,0 +1,23 @@
+import { memo, SVGProps } from 'react'
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="none"
+    stroke="#fbff00"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#fbff00"
+      fillRule="evenodd"
+      d="m15 9-3-7-3 7-7 1 5 4-1 7 6-3 6 3-1-7 5-4-7-1Zm4 2-5-1-2-4-2 4-5 1 4 3-1 5 4-3 4 3-1-5 4-3Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+const StarEmpty = memo(SvgComponent)
+
+export default StarEmpty
