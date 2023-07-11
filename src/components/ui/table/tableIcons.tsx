8631845a4718ee61ddf93a5cbd895e818b1e145d
Yelkira <penda.fu@gmail.com>
2023-07-11 18:28:34 +0300

    add icons component

---
+++ src/components/ui/table/tableIcons.tsx
@@ -0,0 +1,19 @@
+import { Edit } from '../../../assets/icons/Edit.tsx'
+import { Play } from '../../../assets/icons/Play.tsx'
+import { Trash } from '../../../assets/icons/Trash.tsx'
+
+export const TablePackIcons = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+      <button style={{ cursor: 'pointer' }} onClick={() => alert('ad')}>
+        <Play />
+      </button>
+      <button style={{ cursor: 'pointer' }} onClick={() => alert('ad')}>
+        <Edit />
+      </button>
+      <button style={{ cursor: 'pointer' }} onClick={() => alert('ad')}>
+        <Trash />
+      </button>
+    </div>
+  )
+}
