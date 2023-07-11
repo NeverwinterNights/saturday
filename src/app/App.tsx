e9916e2fcb71eef5b326152e3c08ffb3baa43029
Igor Shargin <igor20513@gmail.com>
2023-07-11 16:25:43 +0500

    feat: add cards, packs, profile, learn pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,10 +16,14 @@ export function App() {
       <div style={{ display: 'flex', color: 'white', gap: 20, justifyContent: 'center' }}>
         <Link to={PATH.LOGIN}>Login</Link>
         <Link to={PATH.REGISTRATION}>Register</Link>
-        <Link to={PATH.PROFILE}>Profile</Link>
         <Link to={PATH.NEW_PASSWORD}>New password</Link>
         <Link to={PATH.PASSWORD_RECOVERY}>password recovery</Link>
         <Link to={PATH.CHECK_EMAIL}>Check email</Link>
+        <Link to={PATH.PACKS}>packs</Link>
+        <Link to={PATH.PACKS + PATH.CARDS}>Cards</Link>
+        <Link to={PATH.LEARN + '/2'}>learn</Link>
+        <Link to={PATH.PROFILE}>Profile</Link>
+        <Link to={PATH.ERROR}>Error</Link>
       </div>
       <Pages />
     </>
