342857bf9cc5d28d3982ba851c7d5c3b23a2eb6f
Yelkira <penda.fu@gmail.com>
2023-07-19 20:52:34 +0300

    add logic to password recovery

--- src/features/auth/components/password-recovery/password-recovery.tsx
+++ src/features/auth/components/password-recovery/password-recovery.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
+import { PATH } from '@/common'
 import { ForgotPassword } from '@/components/auth/forgot-password-form'
+import { ForgotPasswordFormType } from '@/components/auth/forgot-password-form/use-forgot-password.ts'
+import { useRecoverPasswordMutation } from '@/features/auth/service/api/auth.api.ts'
 
 export const ForgotPasswordPage = () => {
-  return <ForgotPassword onSubmitHandler={e => console.log('на вашу почту отправлено письмо', e)} />
+  const navigate = useNavigate()
+  const [recoverPassword] = useRecoverPasswordMutation()
+
+  const recoverPasswordHandler = async (data: ForgotPasswordFormType) => {
+    await recoverPassword(data)
+      .unwrap()
+      .then(() => {
+        navigate(PATH.CHECK_EMAIL)
+      })
+      .catch(e => {
+        throw new Error(e)
+      })
+  }
+
+  return <ForgotPassword onSubmitHandler={recoverPasswordHandler} />
 }
