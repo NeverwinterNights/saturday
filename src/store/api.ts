82ca01dadf7bdd3fe9098d6c663362f28b9cd191
Igor Shargin <igor20513@gmail.com>
2023-07-20 14:41:15 +0500

    fix: delete localStorage logic

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,59 +1,24 @@
-import {
-  BaseQueryFn,
-  FetchArgs,
-  fetchBaseQuery,
-  FetchBaseQueryError,
-} from '@reduxjs/toolkit/query/react'
+import { BaseQueryFn, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseUrl = 'https://andri-flashcards-api.onrender.com/v1/'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: baseUrl,
-  // credentials: 'include',
-  prepareHeaders: headers => {
-    const accessToken = localStorage.getItem('token')
-
-    if (accessToken) {
-      headers.set('authorization', `Bearer ${accessToken}`)
-    }
-
-    return headers
-  },
+  credentials: 'include',
 })
 
-export const baseQueryWithReauth: BaseQueryFn<
-  string | FetchArgs,
-  unknown,
-  FetchBaseQueryError
-> = async (args, api, extraOptions) => {
+export const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions)
 
   if (result.error && result.error.status === 401) {
-    const refreshResult = (await baseQuery(
+    await baseQuery(
       {
         url: 'auth/refresh-token',
         method: 'POST',
       },
       api,
       extraOptions
-    )) as { data: { accessToken: string } }
-
-    if (refreshResult.data.accessToken) {
-      localStorage.setItem('token', refreshResult.data.accessToken as string)
-
-      // retry the initial query
-      result = await baseQuery(args, api, extraOptions)
-    } else {
-      await baseQuery(
-        {
-          url: 'auth/logout',
-          method: 'POST',
-        },
-        api,
-        extraOptions
-      )
-      localStorage.setItem('token', '')
-    }
+    )
   }
 
   return result
