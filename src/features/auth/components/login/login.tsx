d52567c21f6d0d42080dc10ff5c2174f8f2f17ff
Igor Shargin <igor20513@gmail.com>
2023-07-22 23:36:21 +0500

    feat: add func navigate to Packs

--- src/features/auth/components/login/login.tsx
+++ src/features/auth/components/login/login.tsx
@@ -3,11 +3,12 @@ import { useNavigate } from 'react-router-dom'
 import { PATH } from '@/common'
 import { LoginForm } from '@/components/auth/login-form'
 import { LoginFormType } from '@/components/auth/login-form/use-login-form.ts'
-import { useLoginMutation, util } from '@/features/auth/service/api/auth.api.ts'
+import { useLoginMutation, useMeQuery, util } from '@/features/auth/service/api/auth.api.ts'
 import { useAppDispatch } from '@/store/store.ts'
 
 export const LoginPage = () => {
   const navigate = useNavigate()
+  const { data } = useMeQuery()
   const [login] = useLoginMutation()
   const dispatch = useAppDispatch()
   const loginHandler = async (data: LoginFormType) => {
@@ -20,5 +21,6 @@ export const LoginPage = () => {
       .catch(error => console.log(error))
   }
 
-  return <LoginForm onSubmitHandler={loginHandler} />
+  if (!data) return <LoginForm onSubmitHandler={loginHandler} />
+  if (data) navigate(PATH.PACKS)
 }
