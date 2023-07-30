4d4dff57e01cd2a26e1709f953e172eef1871f7a
Yelkira <penda.fu@gmail.com>
2023-07-30 22:27:51 +0300

    fixed icon display

--- src/assets/icons/AddMediaIcon.tsx
+++ src/assets/icons/AddMediaIcon.tsx
@@ -9,7 +9,7 @@ export const AddMediaIcon = (props: SVGProps<SVGSVGElement>) => (
     fill="none"
     {...props}
   >
-    <g fill="white" clipPath="url(#a)">
+    <g fill="white">
       <path d="M12.5 2h-8a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2Zm-8 1.33h8a.67.67 0 0 1 .67.67v5.57l-2.14-1.82a1.85 1.85 0 0 0-2.34 0L3.83 11.8V4a.67.67 0 0 1 .67-.67Zm8 9.34H4.87l4.67-3.9a.52.52 0 0 1 .62 0l3 2.56V12a.67.67 0 0 1-.66.67Z" />
       <path d="M5.83 6.67a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" />
     </g>
