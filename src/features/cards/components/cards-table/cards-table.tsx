299fbaef0846fdf841bb4be5ad17cd39bc6a1d5d
Yelkira <penda.fu@gmail.com>
2023-07-30 22:14:58 +0300

    some fixes in translate

--- src/features/cards/components/cards-table/cards-table.tsx
+++ src/features/cards/components/cards-table/cards-table.tsx
@@ -9,6 +9,7 @@ import { Table } from '@/components/ui/table/table.tsx'
 import { Sort, TableHeader, TableHeaderType } from '@/components/ui/table-header/table-header.tsx'
 import { useDeleteCardMutation } from '@/features/cards/service/api/cards.api.ts'
 import { CardType } from '@/features/packs/service/api/packs.types.ts'
+import { useTranslate } from '@/i18n.ts'
 
 type PropsType = {
   cardsData: CardType[]
@@ -17,32 +18,33 @@ type PropsType = {
 }
 
 export const CardsTable: FC<PropsType> = ({ cardsData, onSort, sort }) => {
+  const t = useTranslate()
   const myId = '0000'
   const headers: TableHeaderType[] = [
     {
       key: 'question',
-      label: 'Question',
+      label: t('Question'),
       className: `${s.question}`,
       show: true,
       isSortable: true,
     },
     {
       key: 'answer',
-      label: 'Answer',
+      label: t('Answer'),
       show: true,
       className: `${s.answer}`,
       isSortable: true,
     },
     {
       key: 'updated',
-      label: 'Last updated',
+      label: t('Last updated'),
       className: `${s.lastUpdatedMy} ${myId === cardsData[0]?.userId ? s.lastUpdatedOther : ''}`,
       show: true,
       isSortable: true,
     },
     {
       key: 'grade',
-      label: 'Grade',
+      label: t('Grade'),
       className: `${s.gradeMy} ${myId === cardsData[0]?.userId ? s.gradeOther : ''}`,
       show: true,
       isSortable: true,
