53fe7318c3c7411f7543f777c730a8c27c3d5306
Yelkira <penda.fu@gmail.com>
2023-07-31 20:28:23 +0300

    inject loader in project

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,14 @@
 import { Outlet, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
+import { MainLoader } from '@/assets/loaders/main-loader.tsx'
 import { PATH } from '@/common'
 import { Header } from '@/components/ui/header'
 import { useLogoutMutation, useMeQuery, util } from '@/features/auth/service/api/auth.api.ts'
 import { useAppDispatch } from '@/store/store.ts'
 
 export const Layout = () => {
-  const { data } = useMeQuery()
+  const { data, isLoading } = useMeQuery()
   const [logout] = useLogoutMutation()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
@@ -26,16 +27,22 @@ export const Layout = () => {
 
   return (
     <div>
-      <Header
-        isAuth={!!data}
-        email={data?.email}
-        name={data?.name}
-        avatar={data?.avatar}
-        onSignOut={onSignOut}
-        onSignIn={() => navigate(PATH.LOGIN)}
-        onProfileClick={() => navigate(PATH.PROFILE)}
-      />
-      <Outlet />
+      {isLoading ? (
+        <MainLoader />
+      ) : (
+        <>
+          <Header
+            isAuth={!!data}
+            email={data?.email}
+            name={data?.name}
+            avatar={data?.avatar}
+            onSignOut={onSignOut}
+            onSignIn={() => navigate(PATH.LOGIN)}
+            onProfileClick={() => navigate(PATH.PROFILE)}
+          />
+          <Outlet />
+        </>
+      )}
     </div>
   )
 }
