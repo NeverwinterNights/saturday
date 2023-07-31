84233c8fbb09e89e1a994bf5fa95d753dbd0a4ff
Yelkira <penda.fu@gmail.com>
2023-07-31 22:21:20 +0300

    fixed icon display

--- src/assets/icons/CloseInputIcon.tsx
+++ src/assets/icons/CloseInputIcon.tsx
@@ -1,7 +1,7 @@
 import { SVGProps, memo } from 'react'
 export const ClosedInputIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} {...props}>
-    <g clipPath="url(#a)">
+    <g>
       <path
         fill="#fff"
         d="m8.94 8 2.87-2.86a.67.67 0 0 0-.95-.95L8 7.06 5.14 4.19a.67.67 0 0 0-.95.95L7.06 8l-2.87 2.86A.67.67 0 0 0 4.67 12a.67.67 0 0 0 .47-.2L8 8.95l2.86 2.87a.67.67 0 1 0 .95-.95L8.94 8Z"
