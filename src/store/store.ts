aa4c42c24976138502897bd4f4f04cffe48fcbab
Yelkira <penda.fu@gmail.com>
2023-07-31 21:52:46 +0300

    add app slice for loaders

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
+import { appReducer } from '@/app/app.slice.ts'
 import { authAPI } from '@/features/auth/service/api/auth.api.ts'
 import { cardAPI } from '@/features/cards/service/api/cards.api.ts'
 import { decksAPI } from '@/features/packs/service/api/packs.api.ts'
@@ -9,6 +10,7 @@ import { profileAPI } from '@/features/profile/service/api/profile.api.ts'
 
 export const store = configureStore({
   reducer: {
+    appReducer,
     [authAPI.reducerPath]: authAPI.reducer,
     [profileAPI.reducerPath]: profileAPI.reducer,
     [cardAPI.reducerPath]: cardAPI.reducer,
