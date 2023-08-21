7a236956bc588fb0775ef11e904622917bc3d6b6
Yelkira <penda.fu@gmail.com>
2023-08-21 14:14:30 +0300

    add packs.slice

---
+++ src/features/packs/service/packs.selectors.ts
@@ -0,0 +1,12 @@
+import { RootState } from '@/store/store.ts'
+
+export const selectDecksPage = (state: RootState) => state.decksReducer.page
+export const selectDecksPageSize = (state: RootState) => state.decksReducer.pageSize
+export const selectDecksSort = (state: RootState) => state.decksReducer.sort
+export const selectDeckNameToSearch = (state: RootState) => state.decksReducer.nameToSearch
+export const selectTabValue = (state: RootState) => state.decksReducer.tabValue
+export const selectRange = (state: RootState) => state.decksReducer.range
+export const selectIsMaxCardsCountInit = (state: RootState) =>
+  state.decksReducer.isMaxCardsCountInit
+export const selectMinSlider = (state: RootState) => state.decksReducer.minSlider
+export const selectMaxSlider = (state: RootState) => state.decksReducer.maxSlider
