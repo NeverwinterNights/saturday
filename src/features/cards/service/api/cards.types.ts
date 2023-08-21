c8a11473f890a7fd240973d1690f10416e02cc9b
pavelcardash <cardash@mail.ru>
2023-08-21 11:59:09 +0300

    fix placeholder input

--- src/features/cards/service/api/cards.types.ts
+++ src/features/cards/service/api/cards.types.ts
@@ -1,10 +1,10 @@
 export type UpdateCardByIdRequestType = FormData
 
-// export type UpdateCardByIdRequestType = {
-//   questionImg?: string
-//   answerImg?: string
-//   question?: string
-//   answer?: string
-//   questionVideo?: string
-//   answerVideo?: string
-// }
+export type RequestType = {
+  questionImg?: string
+  answerImg?: string
+  question?: string
+  answer?: string
+  questionVideo?: string
+  answerVideo?: string
+}
